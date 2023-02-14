1417a0679cfb0b40a93e3d87e9848501a9896b9c
Rajmund Zawiślak <rajmund.zawislak@neontri.com>
2023-02-14 11:07:37 +0100

    Add project

---
+++ Brawl Stars Insider/Brawl_Stars_InsiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brawl_Stars_InsiderApp.swift
+//  Brawl Stars Insider
+//
+//  Created by Rajmund Zawiślak on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Brawl_Stars_InsiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
